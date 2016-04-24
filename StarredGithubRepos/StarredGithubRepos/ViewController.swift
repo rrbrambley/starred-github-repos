aac484854df527bb1bf2c425d0efab521fef5500
Robert Brambley <brambley@pizzadogz.local>
2016-04-24 11:04:43 -0700

    Add OctoKit, proof-of-concept most-starred request.
    We can't use the OctoKit Cocoapod because the pod spec
    hasn't been updated to point to a version that contains
    the search functionality. OctoKit as a framework dependency
    thus requires several other project dependencies. See
    instructions on README of OctoKit for details.

--- StarredGithubRepos/StarredGithubRepos/ViewController.swift
+++ StarredGithubRepos/StarredGithubRepos/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 AATT. All rights reserved.
 //
 
+import OctoKit
 import UIKit
 
 class ViewController: UIViewController {
@@ -19,7 +20,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        fetchStarredRepositories { (repositories: Array<OCTRepository>) in
+            for repository in repositories {
+                print("repository: \(repository.name) \(repository.stargazersCount)")
+            }
+        }
+    }
 
-
+    func fetchStarredRepositories(completion: (Array<OCTRepository> -> Void)!) {
+        let user:OCTUser = OCTUser.init(rawLogin: "login", server: OCTServer.dotComServer())
+        let client:OCTClient = OCTClient.unauthenticatedClientWithUser(user)
+        
+        var repositories:Array<OCTRepository> = Array<OCTRepository>()
+    
+        client.searchRepositoriesWithQuery("stars:>0", orderBy:"stars", ascending:false).subscribeNext(
+            { (repositorySearchResults: AnyObject!) -> Void in
+                repositories.appendContentsOf(repositorySearchResults.repositories as! Array)//(repository as? OCTRepository)!)
+            },
+            error: { (error: NSError!) -> Void in
+                NSLog(error.localizedDescription)
+            },
+            completed: {
+                if(completion != nil) {
+                    completion(repositories)
+                }
+            }
+        );
+    }
 }
 
